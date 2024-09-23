5c8123c78e064ee94a2ae07faf8b95cb9f086a82
= <avery_lin@intuit.com>
2024-09-23 19:10:41 -0400

    setup supabase/packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='budget-cli',           
+    version='1.0.0',            
+    packages=find_packages(),    
+    install_requires=[           
+        'supabase',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'budget = budget.budget:main',  
+        ],
+    },
+)
